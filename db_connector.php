375e0d45179ea27829ce91831e767b852b40c5c8
banchomin <ban.chomin@gmail.com>
2024-01-05 16:42:40 +0900

    1/5 DB 만들기, 연결, 데이터 가져오기

--- db_connector.php
+++ db_connector.php
@@ -10,12 +10,11 @@ $DB_PORT = '3306';
 // DB name
 $DB_NAME = 'instagram';
 // LINK
-$DB_LINK = "mysql:host = {$DB_HOST}; port = {$DB_PORT}; dbname = {$DB_NAME};";
+$DB_LINK = "mysql:host={$DB_HOST};port={$DB_PORT};dbname={$DB_NAME};"; // without space
 
 // DB connect module = PDO, 예외 처리
 try {
     $db = new PDO($DB_LINK, $DB_ID, $DB_PW);
-    echo "DB 연결 성공";
 } catch (PDOException $e) {
     echo "<script>alert('DB 연결에 실패하였습니다.')</script>";
 }
@@ -24,5 +23,6 @@ try {
 $query = "SELECT * FROM `user`";
 $result = $db->prepare($query);
 $result->execute();
-$result->fetchAll(PDO::FETCH_ASSOC); // 리스트 형태로 가져와라(PDO의 한 열 씩)
+$data = $result->fetchAll(PDO::FETCH_ASSOC); // 리스트 형태로 가져와라(PDO의 한 열 씩 방식으로)
+echo json_encode($data);
 ?>
